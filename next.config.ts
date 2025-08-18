cecad75a77112090393427cfa5f4ef98fb75feeb
Willian Alves Moreno <willianam@uninove.br>
2025-08-18 19:17:01 -0300

    feat: Configuração do pageExtensions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
 }
 
 export default nextConfig

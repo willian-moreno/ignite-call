22abdd8506fce7777abe8ed047e9052141fbd93c
Willian Alves Moreno <willianam@uninove.br>
2025-08-19 14:15:01 -0300

    feat: Estrutura visual da home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import previewImage from '@/assets/app-preview.png'
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading
+          as="h1"
+          size="4xl"
+        >
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          height={400}
+          quality={100}
+          alt="Calendário simbolizando aplicação em funcionamento"
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,11 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    <>
-      <Heading as="h1" size="2xl">
-        Hello World
-      </Heading>
-    </>
-  )
-}
+export { default } from '@/pages/home'

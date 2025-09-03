12c523c7a3a85056c3803d953b64585445adebb6
Willian Alves Moreno <willianam@uninove.br>
2025-09-03 19:38:07 -0300

    feat: Configurando o Next Auth

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,6 +1,7 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 // import { api } from "../../../lib/axios"
+import { signIn } from 'next-auth/react'
 import { Container, Header } from '../styles'
 import { ConnectBox, ConnectItem } from './styles'
 
@@ -30,6 +31,7 @@ export default function Register() {
           <Button
             variant="secondary"
             size="sm"
+            onClick={() => signIn('google')}
           >
             Conectar
             <ArrowRight />

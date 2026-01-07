de78ad63aae16c169f608ca995a2c727012c0562
Willian Alves Moreno <willianam@uninove.br>
2026-01-07 16:58:00 -0300

    feat: Persistindo intervalos

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,6 +1,18 @@
+import { prisma } from '@/lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
+import z from 'zod'
 import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number().min(0).max(6),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,5 +28,24 @@ export default async function handler(
     buildNextAuthOptions(req, res),
   )
 
-  return res.json({ session })
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map(async (interval) => {
+      await prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }

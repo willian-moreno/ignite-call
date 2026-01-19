18e24e41539a46d6b91ac263c5c2a343e1ebb131
Willian Alves Moreno <willianam@uninove.br>
2026-01-19 17:50:51 -0300

    feat: Página de confirmação

--- src/pages/schedule/[username]/schedule-form/confirm-step/index.tsx
+++ src/pages/schedule/[username]/schedule-form/confirm-step/index.tsx
@@ -1,3 +1,61 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
 export function ConfirmStep() {
-  return <></>
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm
+      as="form"
+      onSubmit={handleConfirmScheduling}
+    >
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput
+          placeholder="Seu nome"
+          onPointerEnterCapture
+          onPointerLeaveCapture
+          crossOrigin="anonymous"
+        />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput
+          type="email"
+          placeholder="johndoe@example.com"
+          onPointerEnterCapture
+          onPointerLeaveCapture
+          crossOrigin="anonymous"
+        />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button
+          type="button"
+          variant="tertiary"
+        >
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }

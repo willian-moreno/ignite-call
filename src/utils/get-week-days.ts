c7c8b396bdbebe7a33da70a197add703a389b2af
Willian Alves Moreno <willianam@uninove.br>
2026-01-19 16:06:17 -0300

    feat: Estrutura visual do calendário

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,9 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
   return Array.from({ length: 7 }, (_, day) => {
     const date = new Date(Date.UTC(2021, 5, day))
     const weekDay = formatter.format(date)
+
+    if (short) {
+      return weekDay.substring(0, 3).toUpperCase()
+    }
 
     return weekDay.charAt(0).toUpperCase() + weekDay.slice(1)
   })

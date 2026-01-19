191daa01abf7cb573b24833d7a960eda2933ccb9
Willian Alves Moreno <willianam@uninove.br>
2026-01-19 16:36:38 -0300

    feat: Estrutura visual da lista de horários

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,21 @@ export const globalStyles = globalCss({
     margin: 0,
   },
 
+  '*::-webkit-scrollbar': {
+    width: '$2',
+    height: '$2',
+  },
+
+  '*::-webkit-scrollbar-track': {
+    background: '$gray800',
+  },
+
+  '*::-webkit-scrollbar-thumb': {
+    border: '1px solid $gray800',
+    borderRadius: '9999px',
+    background: '$gray200',
+  },
+
   body: {
     backgroundColor: '$gray900',
     color: '$gray100',
